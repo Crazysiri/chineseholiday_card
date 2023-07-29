798244e63e5c6002012849843f9358eb4c2b5589
WalterDSU <91654066+WalterDSU@users.noreply.github.com>
2023-07-29 17:07:45 +0800

    修复相近纪念日展示的排序不能按从最近日期排列在前的问题

--- dist/ch_calendar-card.js
+++ dist/ch_calendar-card.js
@@ -363,6 +363,11 @@ class ChineseCalendarCard extends LitElement {
 
     }
 
+	  
+    if (beAdd) {
+      list.push(beAdd);
+    }
+
     if (attributes.hasOwnProperty('next_anniversaries')) {
         var next_anniversaries = attributes['next_anniversaries'];
         for (var i = 0; i < next_anniversaries.length;i++) {
@@ -371,10 +376,6 @@ class ChineseCalendarCard extends LitElement {
         }    
     }
 
-
-    if (beAdd) {
-      list.push(beAdd);
-    }
 
     if (attributes.hasOwnProperty('future_dates')) {
       var future_dates = attributes['future_dates'];
